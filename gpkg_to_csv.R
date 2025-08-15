0bfc48df08a4e2283e1bd84093437344dedc8198
veragotze <157293814+veragotze@users.noreply.github.com>
2025-08-15 10:35:58 -0700

    Update split view

--- gpkg_to_csv.R
+++ gpkg_to_csv.R
@@ -1,9 +1,19 @@
+#combine datasets for leaflet
 library(sf)
 library(dplyr)
 
 # Read the GeoPackage layer
-polygons_sf <- st_read("~/Documents/SUBDENSE/Projects/CompareDrivers_Countries/R Outputs/str_ha_drivers.gpkg") %>% 
-  st_transform(crs = 3035)
+str_sf <- st_read("C:/Users/Vera/Documents/SUBDENSE/Projects/CompareDrivers_Countries/R Outputs/str_ha_drivers.gpkg") %>% 
+  st_transform(crs = 3035) %>%
+  select(c(amenity_count, m_to_train, m_to_park, total_area2011, cci, total_volume_new))
+dor_sf <- st_read("C:/Users/Vera/Documents/SUBDENSE/Projects/CompareDrivers_Countries/R Outputs/dor_ha_drivers.gpkg") %>% 
+  st_transform(crs = 3035) %>%
+  select(c(amenity_count, m_to_train, m_to_park, total_area2011, cci, total_volume_new))
+liv_sf <- st_read("C:/Users/Vera/Documents/SUBDENSE/Projects/CompareDrivers_Countries/R Outputs/liv_ha_drivers.gpkg") %>% 
+  st_transform(crs = 3035) %>%
+  select(c(amenity_count, m_to_train, m_to_park, total_area2011, cci, total_volume_new))
+
+polygon_sf <- bind_rows(str_sf, dor_sf, liv_sf)
 
 # Function to get the lower-left coordinates of the bounding box
 get_lower_left <- function(geom) {
@@ -12,7 +22,7 @@ get_lower_left <- function(geom) {
 }
 
 # Apply the function to each geometry to get the lower-left coordinates
-lower_left_coords <- st_geometry(polygons_sf) %>%
+lower_left_coords <- st_geometry(polygon_sf) %>%
   lapply(get_lower_left) %>%
   do.call(rbind, .) %>%
   as_tibble()
@@ -22,10 +32,10 @@ lower_left_coords <- lower_left_coords %>%
   rename(x = x.xmin, y = y.ymin)
 
 # Combine the coordinates with the original attributes (excluding geometry)
-polygon_attributes <- polygons_sf %>%
+polygon_attributes <- polygon_sf %>%
   st_drop_geometry()
 
 final_data <- bind_cols(polygon_attributes, lower_left_coords)
 
 # Write the data to a CSV file
-write.csv(final_data, "~/Documents/SUBDENSE/Projects/CompareDrivers_Countries/leaflet_subsense/strasbourg.csv", row.names = FALSE)
+write.csv(final_data, "C:/Users/Vera/Documents/SUBDENSE/Projects/CompareDrivers_Countries/drivers_leaflet/drivers.csv", row.names = FALSE)
